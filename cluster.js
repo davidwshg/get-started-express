a6118478e4281d818a89684c1129ec88bd24caf7
David <davidwashington833@gmail.com>
2018-05-26 14:50:31 -0300

    Usando todos os cores do precessador

---
+++ cluster.js
@@ -0,0 +1,26 @@
+const cluster = require('cluster');
+const os = require('os');
+
+const logger = require('./services/winston');
+
+const cpus = os.cpus()
+
+logger.info('executed thread');
+
+if (cluster.isMaster) {
+  logger.info('thread master');
+
+  cpus.forEach(() => cluster.fork());
+
+  cluster.on('listening', (worker) =>
+    logger.info('cluster conected', worker.process.pid));
+
+  cluster.on('exit', (worker) => {
+    logger.info('cluster %d disconnected', worker.process.pid)
+    cluster.fork()
+  })
+}
+else {
+  logger.info('thread slave');
+  require('./app.js');
+}
