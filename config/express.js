43b6faae4d93e1786a389ba2d3339394c47ac597
David <davidwashington833@gmail.com>
2018-05-26 14:16:23 -0300

    Configurando o consign e criando primeiro controller

--- config/express.js
+++ config/express.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const consign = require('consign');
 
 const app = express();
+
+consign()
+  .include('services')
+  .then('controllers')
+  .then('routes')
+  .into(app);
 
 module.exports = app;
---
+++ controllers/value.js
@@ -0,0 +1,12 @@
+module.exports = (app) => {
+
+  const value = {};
+
+  value.get = (req, res) => {
+    const values = ['v1', 'v2', 'v3'];
+
+    res.status(200).send(values);
+  }
+
+  return value;
+}
