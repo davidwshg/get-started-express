cf7e033d1c292d2969969a81a01c4658a01e65ea
davidwashington833 <davidwashington833@gmail.com>
2018-08-19 17:51:27 -0300

    Promise to Async

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -2,44 +2,52 @@ import db from '../services/sequelize';
 
 class User {
 
-  get(req, res) {
-    db.user
-      .findAll()
-      .then(user => res.status(200).send(user))
-      .catch(err => res.status(500).send(err));
+  async get(req, res) {
+    try {
+      const users = await db.user.findAll();
+      res.status(200).send(users);
+    } catch (error) {
+      res.status(500).send(error);
+    }
   }
 
-  getForId(req, res) {
-    db.user
-      .findOne({ where: { id: req.params.id } })
-      .then(user => res.status(200).send(user))
-      .catch(err => res.status(500).send(err));
+  async getForId(req, res) {
+    try {
+      const user = await db.user.findOne({ where: { id: req.params.id } });
+      res.status(200).send(user);
+    } catch (error) {
+      res.status(500).send(error);
+    }
   }
 
-  create(req, res) {
-    const { firstName, lastName } = req.body;
-
-    db.user
-      .create({ firstName, lastName })
-      .then(user => res.status(200).send(user))
-      .catch(err => res.status(500).send(err));
+  async create(req, res) {
+    try {
+      const { firstName, lastName } = req.body;
+      const user = await db.user.create({ firstName, lastName });
+      res.status(200).send(user);
+    } catch (error) {
+      res.status(500).send(error);
+    }
   }
 
-  update(req, res) {
-    const { firstName, lastName } = req.body;
-
-    db.user
-      .find({ where: { id: req.params.id } })
-      .then(user => user.updateAttributes({ firstName, lastName }))
-      .then(user => res.status(200).send(user))
-      .catch(err => res.status(500).send(err));
+  async update(req, res) {
+    try {
+      const { firstName, lastName } = req.body;
+      const user = await db.user.find({ where: { id: req.params.id } });
+      user.updateAttributes({ firstName, lastName });
+      res.status(200).send(user);
+    } catch (error) {
+      res.status(500).send(error);
+    }
   }
 
-  delete(req, res) {
-    db.user
-      .destroy({ where: { id: req.params.id } })
-      .then(res => res.status(200).send(res))
-      .catch(err => res.status(500).send(err));
+  async delete(req, res) {
+    try {
+      const user = await db.user.destroy({ where: { id: req.params.id } });
+      res.status(200).send({});
+    } catch (error) {
+      res.status(500).send(error)
+    }
   }
 
 }
