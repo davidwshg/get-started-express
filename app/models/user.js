eff351b1fca92faba31dfd76fe444ab310c0e2dd
davidwashington833 <davidwashington833@gmail.com>
2018-08-19 17:55:47 -0300

    Change config of the server and model user

--- app/models/user.js
+++ app/models/user.js
@@ -1,12 +1,18 @@
 export default (sequelize, Sequelize) => {
-  const User = sequelize.define('user', {
-    firstName: {
-      type: Sequelize.STRING
+  const User = sequelize.define('user',
+    {
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      }
     },
-    lastName: {
-      type: Sequelize.STRING
+    {
+      timestamps: false,
+      freezeTableName: true
     }
-  });
+  );
 
   User.sync({ force: true }).then(() => {
     // Table created
